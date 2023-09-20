fdbc9554b1146bc4151437c4f13090f73bd1b244
young <ykmaeng@gmail.com>
2023-09-21 01:29:06 +1200

    Add an image of challenge4

--- src/features/exercise/Challenge4.tsx
+++ src/features/exercise/Challenge4.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
-const requirements = `
+const requirements1 = `
+Using an online tool like:
+*	https://stackblitz.com/
+*	https://codepen.io/
+*	etc…
+ 
+to design the following react component:
+`;
+
+const requirements2 = `
+Notes:
+*	it needs to load inside a modal view
+*	filters & pagination are desired but not needed.
+*	the submission does not need to match the UI styles, however, the closer it gets the better.
+*	create and load mock data from a json file.
+
 `;
 
 export default function Challenge4() {
@@ -10,7 +25,13 @@ export default function Challenge4() {
       <h1>Challenge 4</h1>
       <details>
         <summary>Requirements</summary>
-        <ReactMarkdown children={requirements} />
+        <ReactMarkdown children={requirements1} />
+        <img
+          style={{ width: "100%", height: "auto", maxWidth: "1024px" }}
+          src="https://github.com/swisex/swisex-front/blob/9e8b3e827acf9f5dddf68fb7ad35aa84c56da68a/src/features/exercise/challenge3.png?raw=true"
+          alt="challenge 4"
+        />
+        <ReactMarkdown children={requirements2} />
       </details>
     </>
   );
