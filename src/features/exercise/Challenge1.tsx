1464bcd8b7abf48328935d25eb126694d4eb363c
young <ykmaeng@gmail.com>
2023-09-21 00:11:30 +1200

    Fix syntax hightlight language

--- src/features/exercise/Challenge1.tsx
+++ src/features/exercise/Challenge1.tsx
@@ -167,7 +167,7 @@ export default function Challenge1() {
           code({ node, inline, className, children, ...props }) {
             const match = /language-(\w+)/.exec(className || "");
             return !inline && match ? (
-              <SyntaxHighlighter language="typescript">
+              <SyntaxHighlighter language="sql">
                 {String(children).replace(/\n$/, "")}
               </SyntaxHighlighter>
             ) : (
